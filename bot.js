df09b0378dc69aa93a08ecbf3b69974d66512631
Robben9874 <37851177+Robben9874@users.noreply.github.com>
2018-11-30 16:09:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`اسم سيرفرك`,"http://twitch.tv/S-F")
+client.user.setGame(`OBX`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
